a23e0398ef72721be896feea8e9e8dbedc49e097
Rayyan Shaikh <67497096+Rayyansh@users.noreply.github.com>
2024-11-15 11:59:23 +0530

    Update settings.py

--- EcommerceVendors/settings.py
+++ EcommerceVendors/settings.py
@@ -81,9 +81,9 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = 'static/'
-STATIC_ROOT = '/EcommerceVendors/static/'
+STATIC_ROOT = '/Ecom/static/'
 
-MEDIA_ROOT = '/EcommerceVendors/media/'
+MEDIA_ROOT = '/Ecom/media/'
 MEDIA_URL = 'media/'
 
 STATICFILES_DIRS = [BASE_DIR / 'static']
