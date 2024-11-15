86415f96adfd679cfa064d4b1ec7371248bd61e7
Rayyan Shaikh <67497096+Rayyansh@users.noreply.github.com>
2024-11-15 12:23:36 +0530

    Update settings.py

--- EcommerceVendors/settings.py
+++ EcommerceVendors/settings.py
@@ -87,7 +87,7 @@ STATIC_ROOT = 'static/'
 MEDIA_ROOT = 'media/'
 MEDIA_URL = 'media/'
 
-STATICFILES_DIRS = [BASE_DIR / 'static']
+# STATICFILES_DIRS = [BASE_DIR / 'static']
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
